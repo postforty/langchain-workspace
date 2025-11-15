004973f33eb8369e202a04b0781b796ac185a46a
postforty <75767073+postforty@users.noreply.github.com>
2025-11-15 16:52:40 +0900

    7일차 실습코드

---
+++ ch04/sec02/03_word_chain_game_gradio.py
@@ -0,0 +1,105 @@
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=gemini_api_key)
+
+# 대화 기록을 저장할 히스토리 클래스 불러오기
+chat_history = ChatMessageHistory()
+
+chat_history.messages
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """당신은 끝말잇기 게임을 진행하는 AI 챗봇입니다. 아래는 게임 규칙입니다. 당신과 user 의 입력에서 아래 규칙이 꼭 지켜져야 하며, 지키지 않은 사람에게 패배를 알린 뒤, 끝말잇기 게임을 종료합니다.
+                1. 주어진 대화 기록에서 이미 나왔던 단어를 다시 말했을 경우 패배합니다.
+                2. 두음법칙을 허용합니다. (ex. 리 -> 이, 력 -> 역, 락 -> 낙)
+                3. 국어사전에 존재하는 단어이자, 명사여야 합니다.
+                4. 아무런 설명 없이, 끝말잇기 단어만 한글로 한 단어만 출력하세요.
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("user", "{input}"),
+    ]
+)
+
+chain = prompt | llm
+
+chain_with_message_history = RunnableWithMessageHistory(
+    chain,
+    lambda session_id: chat_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
+
+def summarize_messages(chain_input):
+    stored_messages = chat_history.messages
+    if len(stored_messages) == 0:
+        # return False
+        return
+    summarization_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("placeholder", "{chat_history}"),
+            (
+                "user",
+                "위 채팅 메시지는 끝말잇기 게임을 진행한 대화내용입니다. 언급한 단어들만 나열하여 저장해주세요.",
+            ),
+        ]
+    )
+    summarization_chain = summarization_prompt | llm
+
+    # chat_history 에 저장된 대화 기록을 요약프롬프트에 입력 & 결과 저장
+    summary_message = summarization_chain.invoke({"chat_history": stored_messages})
+
+    # chat_history 에 저장되어있던 기록 지우기
+    chat_history.clear()
+
+    # 생성된 새로운 요약내용으로 기록 채우기
+    chat_history.add_message(summary_message)
+
+    # return True
+    return
+
+chain_with_summarization = (
+    # RunnablePassthrough는 LCEL에서 사용, 입력값을 다음 단계로 그대로 통과시키는 역할
+    # assign() 메서드는 체인에 들어오는 딕셔너리에 새로운 키-값 추가
+    RunnablePassthrough.assign(messages_summarized=summarize_messages) # 새로운 키 messages_summarized에 값(True 또는 False) 할당, 이 값은 조건부 요약에 활용 가능
+    | chain_with_message_history
+)
+
+# while True:
+#     user_input = input("🧑 YOUR TURN : ")
+#     if user_input == "종료": break
+#     response = chain_with_summarization.invoke(
+#                 {"input": user_input},
+#                 {"configurable": {"session_id": "unused"}},
+#             )
+#     print("🤖 AI TURN : ", response.content) # AIMessage 객체에서 .content 추출
+
+import gradio as gr
+
+def word_chain_response(message, history):
+    response = chain_with_summarization.invoke(
+            {"input": message},
+            {"configurable": {"session_id": "unused"}},
+        )
+    return response.content
+
+demo = gr.ChatInterface(
+    word_chain_response, 
+    type="messages", 
+    autofocus=False,
+    title="🗣️끝말잇기 게임",
+    description="AI와 함께 끝말잇기 게임을 해보세요! 단어만 입력하면 됩니다."
+    )
+
+if __name__ == "__main__":
+    demo.launch()
---
+++ ch04/sec03/01_streamlit_prompt_maker.py
@@ -0,0 +1,106 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, load_prompt
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+import glob
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if "task_input" not in st.session_state:
+    st.session_state["task_input"] = ""
+
+st.title("🤖 나만의 랭체인 챗봇")
+st.caption("랭체인을 사용하여 챗봇을 만들었습니다.")
+
+with st.sidebar:
+    clear_btn = st.button("초기화")
+
+    prompt_files = glob.glob("prompts/*.yaml")
+    prompt_labels = {
+        'prompts\\general.yaml': "일반 프롬프트", 
+        'prompts\\prompt-maker.yaml': "프롬프트 생성기", 
+        'prompts\\summary.yaml': "요약 프롬프트"
+    }
+
+    selected_prompt = st.selectbox(
+        "프롬프트를 선택해 주세요", 
+        prompt_files, 
+        index=0,
+        format_func=lambda x: prompt_labels.get(x)
+    )
+
+    task_input = st.text_input(
+        "TASK 입력",
+        key="task_input",
+        value=st.session_state["task_input"]
+    )
+
+print("선택한 프롬프트:", selected_prompt)
+print("선택한 프롬프트의 내용:", load_prompt(selected_prompt))
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+def print_messages():
+    for lang_message in st.session_state["messages"]:
+        if lang_message.type == "human":
+            st_role = "user"
+        elif lang_message.type == "ai":
+            st_role = "assistant"
+        else:
+            st_role = "assistant" 
+            
+        st.chat_message(st_role).markdown(lang_message.content)
+
+def add_message(role, message):
+    if role == "user":
+        msg_obj = HumanMessage(content=message)
+    elif role == "assistant":
+        msg_obj = AIMessage(content=message)
+    else:
+        return
+        
+    st.session_state["messages"].append(msg_obj)
+
+def create_chain(prompt_filepath, task=""):
+    prompt = load_prompt(prompt_filepath, encoding="utf-8")
+
+    if task:
+        prompt = prompt.partial(task=task)
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash", google_api_key=gemini_api_key
+    )
+    output_parsers = StrOutputParser()
+
+    chain = prompt | llm | output_parsers
+
+    return chain
+
+if clear_btn:
+    st.session_state["messages"] = []
+
+print_messages()
+
+if prompt := st.chat_input("궁금한 내용을 물어보세요..."):
+    st.chat_message("user").markdown(prompt)
+    add_message("user", prompt)
+
+    chain = create_chain(selected_prompt, task_input)
+
+    response = chain.invoke(
+        {
+            "question": prompt
+        }
+    )
+
+    st.chat_message("assistant").markdown(response)
+
+    add_message("assistant", response)
+
+print(st.session_state.messages)
